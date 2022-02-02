f749577244cb029201fffd11e802f70ac74745a9
Lee Hyun Jun <mikey1000@naver.com>
2022-02-02 23:23:42 +0900

    user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -12,8 +13,27 @@ class User(AbstractUser):
 
     GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"))
 
+    LANGUAGE_ENGLISH = "En"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "En"), (LANGUAGE_KOREAN, "Kr"))
+
+    CURRNECY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRNECY_USD, "usd"), (CURRENCY_KRW, "krw"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birth = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, null=True, blank=True, max_length=2
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, null=True, blank=True, max_length=3
+    )
+
+    superhost = models.BooleanField(default=False)

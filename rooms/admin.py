a89cb566b49e00516bd6e6735476b1d35e312a96
Lee Hyun Jun <mikey1000@naver.com>
2022-02-08 18:14:14 +0900

    using admin panel
    filter_horizen , fieldsets, search_fields, list_filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,7 +11,37 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+
+    fieldsets = (
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces Detail", {"fields": ("amenities", "facilities", "house_rule")}),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = [
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "beds",
+        "baths",
+        "guests",
+        "check_in",
+        "check_out",
+        "instant_book",
+    ]
+
+    list_filter = ["city", "country", "instant_book"]
+
+    search_fields = ["city", "^host__username"]
+
+    filter_horizontal = ["amenities", "facilities", "house_rule"]
 
 
 @admin.register(models.Photo)

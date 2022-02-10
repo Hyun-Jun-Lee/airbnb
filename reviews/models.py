27baf4833ce3380cf2877b1308a78a91ba3792ca
Lee Hyun Jun <mikey1000@naver.com>
2022-02-10 14:13:08 +0900

    make method & register admin

--- reviews/models.py
+++ reviews/models.py
@@ -15,8 +15,23 @@ class Review(core_models.TimeStampedModel):
     check_in = models.IntegerField()
     value = models.IntegerField()
 
-    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room = models.ForeignKey(room_models.Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        user_models.User, related_name="reviews", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        room_models.Room, related_name="reviews", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.review} - {self.room.name}"
+
+    def rating_avg(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg, 2)

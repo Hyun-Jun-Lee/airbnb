74ed34bee3ee116b930092289716b09fe54490b4
Lee Hyun Jun <mikey1000@naver.com>
2022-02-14 15:23:28 +0900

    it's just test

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
+from urllib import request
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+
+
+def all_room(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})

6930a22835c64bd9efb08c344a5d83ee680a444b
Lee Hyun Jun <mikey1000@naver.com>
2022-02-17 14:29:08 +0900

    def get_context_data
    the method how to add context in ListView

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
+from time import timezone
 from urllib import request
+from django.utils import timezone
 from django.shortcuts import redirect, render
 from django.views.generic import ListView
 from . import models
@@ -10,4 +12,10 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
-    
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context

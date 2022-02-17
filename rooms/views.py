87a86217ab3b94a6e6704bc45dcd6ba9b5caea20
Lee Hyun Jun <mikey1000@naver.com>
2022-02-17 16:48:57 +0900

    chango roomdatail view FBV->CBV

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from time import timezone
 from django.utils import timezone
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 # Create your views here.
@@ -21,9 +21,13 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+    model = models.Room
+
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         return redirect(reverse("core:home"))

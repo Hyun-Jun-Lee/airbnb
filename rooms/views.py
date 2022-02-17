5298b0deaebbe213227544c3021970ece1a11a42
Lee Hyun Jun <mikey1000@naver.com>
2022-02-17 14:03:48 +0900

    FBV -> CBV
    using ListView
    it autometically fine templates
    if you want use paginator, it has own paginator
    page->page_obj

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,13 @@
 from urllib import request
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.views.generic import ListView
 from . import models
-from math import ceil
-from django.core.paginator import Paginator, EmptyPage
 
 # Create your views here.
 
 
-def all_room(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            {"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    

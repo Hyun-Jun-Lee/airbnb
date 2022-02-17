4e40a9520196255811a3b05a74a7c06e2e05d4c1
Lee Hyun Jun <mikey1000@naver.com>
2022-02-17 20:56:42 +0900

    make search view by FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from time import timezone
+from django_countries import countries
 from django.utils import timezone
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -31,3 +32,14 @@ class RoomDetail(DetailView):
 #         return render(request, "rooms/detail.html", {"room": room})
 #     except models.Room.DoesNotExist:
 #         return redirect(reverse("core:home"))
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )

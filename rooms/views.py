098f32a15cfd2c2651eeff4cf0a116ebf3a1ca29
Lee Hyun Jun <mikey1000@naver.com>
2022-02-19 20:43:34 +0900

    make filtering with django queryset

--- rooms/views.py
+++ rooms/views.py
@@ -74,6 +74,21 @@ def search(request):
         "amenities": amenities,
         "facilities": facilities,
     }
+
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
+
     return render(
         request,
         "rooms/search.html",

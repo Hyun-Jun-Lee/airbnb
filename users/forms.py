520b0138d867ccdb8c9b85fbc1109e60b42def62
Lee Hyun Jun <mikey1000@naver.com>
2022-02-22 22:35:42 +0900

    add clean_email

--- users/forms.py
+++ users/forms.py
@@ -28,6 +28,14 @@ class SignUpForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")

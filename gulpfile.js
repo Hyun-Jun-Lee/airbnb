ad3e40f7323bab33a84ac6d0f0f4662d930c4086
Lee Hyun Jun <mikey1000@naver.com>
2022-02-27 20:13:00 +0900

    setup for tailwind css

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,11 @@ const css = () => {
     sass.compiler = require("node-sass");
     return gulp
         .src(__dirname + "/assets/scss/styles.scss")
+        // tailwind css를 css로 바꿔주는 라인
         .pipe(sass().on("error", sass.logError))
         .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
         .pipe(minify())
+        // 바꾼 결과를 /static/css 폴더에 저장
         .pipe(gulp.dest("./static/css"));
 };
 exports.default = css;

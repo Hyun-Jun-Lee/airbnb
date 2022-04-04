4a3a4ee7a76be5889d6007cdf8c3b3cd2a3db770
Lee Hyun Jun <mikey1000@naver.com>
2022-04-04 15:37:27 +0900

    room_like

--- rooms/urls.py
+++ rooms/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
     path("create/", views.CreateRoomView.as_view(), name="create"),
     path("search/", views.SearchView.as_view(), name="search"),
+    path("like/<int:room_pk>/", views.room_like, name="like"),
     path("<int:pk>/delete/", views.delete_room, name="delete"),
     path("<int:pk>/edit/", views.EditRoomView.as_view(), name="edit"),
     path("<int:pk>/photos/", views.RoomPhotosView.as_view(), name="photos"),
